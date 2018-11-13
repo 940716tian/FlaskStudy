3db45db270e1da9dcdb6e1468d68a35f8cb0da03
940716tian <mr_xiaohu@126.com>
2018-11-13 11:58:24 +0800

    上午所学“

--- day02/myapp/views.py
+++ day02/myapp/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session
+from flask import Blueprint, session, render_template
 from jinja2 import Template
 
 blue = Blueprint("day02",__name__)
@@ -13,8 +13,28 @@ def get_session():
     res = session.get('name','游客')
     return str(res)
 
-@blue.route('index')
+@blue.route('/index')
 def index():
-    template = Template("<h1>呵呵呵</h1>")
+    import os
+    # 获得当前程序运行所在目录
+    root = os.path.dirname(__file__)
+    # 拼接文件的路径
+    file_path = os.path.join(root,'templates','index.html')
+    f = open(file_path,'r')
+    template = Template(f.read())
     html = template.render()
     return html
+
+@blue.route('/block')
+def my_block():
+    return render_template('test.html')
+
+@blue.route('/my_marco')
+def my_marco():
+    data = ['python','php','C','C++']
+    return render_template('my_marco.html',data=data)
+
+@blue.route('/for_demo')
+def for_demo():
+    data = ['python','php','C','C++']
+    return render_template('fordemo.html',data=data)
